d3cfdbf9037f203206bb9d444440239fe9b007a2
LalehTokhi <lalehtokhi1266@gmail.com>
2026-02-27 11:46:42 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Badge from './Components/Badge';
+
+function App() {
+
+return (
+<div>
+  <Badge label="Course" value="React"></Badge>
+  <Badge label="Week" value="1"></Badge>
+  <Badge label="Topic" value="Props"></Badge>
+  <Badge label="Level" value="Beginner"></Badge>
+</div>
+)
+
+  
+}
+
+export default App;
---
+++ src/Components/Badge.jsx
@@ -0,0 +1,13 @@
+function Badge ({ label, value}) {
+
+return (
+    <div className='style'>
+        <h3>{label}</h3>
+        <p>{value}</p>
+    </div>
+);
+
+
+}
+
+export default Badge;
